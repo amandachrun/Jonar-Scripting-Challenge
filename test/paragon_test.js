0fbe3f19301f672670fc43bed23e6589a8d81ed2
amandachrun <98859155+amandachrun@users.noreply.github.com>
2022-05-09 15:06:46 -0400

    removed parallel testing

--- test/paragon_test.js
+++ test/paragon_test.js
@@ -1,9 +1,17 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-var should = require('chai').should();
+const { Browser } = require('selenium-webdriver'); //required statement allows to pull in specific functionailities
+const {Builder, By, Key, until, WebDriverWait} = require('selenium-webdriver');
+var should = require('chai').should(); //assertion lib
 
-
+//using Mocha
 describe("Registration tests", function(){
     it("Successfully register for a free trial", async function(){
+        //for reuse
+        var company_name = "Starbucks";
+        var first_name = "Kelly";
+        var last_name = "Clarkson";
+        var email = "kelly@email.com";
+        var phone_number = "514-777-8888";
+
         // launches the browser
         let driver = await new Builder().forBrowser('firefox').build();
 
@@ -20,51 +28,74 @@ describe("Registration tests", function(){
         //await driver.get("https://get.paragon-erp.com/learn-more-paragonsmb/");
 
         // fills the fields
-        driver.findElement(By.id("companyname")).sendKeys("Starbucks", Key.RETURN);  
-        driver.findElement(By.id("first_name")).sendKeys("Kelly", Key.TAB);
-        driver.findElement(By.id("last_name")).sendKeys("Clarkson", Key.TAB);
-        driver.findElement(By.id("email")).sendKeys("kelly@email.com", Key.TAB);
+        driver.findElement(By.id("companyname")).sendKeys(company_name, Key.TAB);  
+        driver.findElement(By.id("first_name")).sendKeys(first_name, Key.TAB);
+        driver.findElement(By.id("last_name")).sendKeys(last_name, Key.TAB);
+        driver.findElement(By.id("email")).sendKeys(email, Key.TAB);
         driver.findElement(By.id("industry")).sendKeys("Medical", Key.TAB);
-        driver.findElement(By.id("phone_number")).sendKeys("514-778-3333", Key.TAB);
+        driver.findElement(By.id("phone_number")).sendKeys(phone_number, Key.TAB);
 
 
         //asserts the textbox inputs
-        let company_name = await driver.findElement(By.id("companyname")).getText().then(function(value){
+        let select_company_name = await driver.findElement(By.id("companyname")).getAttribute("value").then(function(value){
             return value;
         });
-        company_name.should.equal("Starbucks");
+        select_company_name.should.equal(company_name);
 
-        var first_name = await driver.findElement(By.id("first_name")).getText().then(function(value){
+        let select_first_name = await driver.findElement(By.id("first_name")).getAttribute("value").then(function(value){
              return value;
         });
-        first_name.should.equal("Kelly");
+        select_first_name.should.equal(first_name);
 
-        var last_name = await driver.findElement(By.id("last_name")).getText().then(function(value){
+        let select_last_name = await driver.findElement(By.id("last_name")).getAttribute("value").then(function(value){
              return value;
         });
-        last_name.should.equal("Clarkson");
+        select_last_name.should.equal(last_name);
 
-        var email = await driver.findElement(By.id("email")).getText().then(function(value){
+        let select_email = await driver.findElement(By.id("email")).getAttribute("value").then(function(value){
             return value;
         });
-        email.should.equal("kelly@email.com");
+        select_email.should.equal(email);
 
-        var industry = await driver.findElement(By.id("industry")).getText().then(function(value){
+        let select_industry = await driver.findElement(By.id("industry")).getAttribute("value").then(function(value){
             return value;
         });
-        industry.should.equal("Medical");
+        //industry.should.equal("Medical");
         
-        var phone_number = await driver.findElement(By.id("phone_number")).getText().then(function(value){
+        let select_phone_number = await driver.findElement(By.id("phone_number")).getAttribute("value").then(function(value){
             return value;
         });
-        phone_number.should.equal("514-778-3333")
+        select_phone_number.should.equal(phone_number)
+
 
         //submits
+        const register_button = await driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[12]/div[2]/form/button"));
+        register_button.click();
 
-        //fills the form
+        //driver.navigate().refresh();
+        try {
+            //asserts the form on the next page
+            let confirm_email = await driver.findElement(By.id('email')).getAttribute("value").then(function(value){
+                return value;
+            });
+            confirm_email.should.equal(email);
 
-        //asserts the form
+            let confirm_company_name = await driver.findElement(By.id("company")).getAttribute("value").then(function(value){
+                return value;
+            });
+            confirm_company_name.should.equal("Starbucks");
 
-        
+            let url = await driver.findElement(By.id('subdomain')).getAttribute("value").then(function(value){
+                return value;
+            });
+            url.should.equal(company_name.toLowerCase());
+        }catch(e) {
+            console.log(e)
+        }
+
+
+        //quits the browser
+        await driver.quit();
     })
+    
 });
